26e7a8e07c7d0ede27d57672cabccc476fbacd6b
Otávio Augusto <otavioaugusto_2007@outlook.com>
2024-11-11 19:33:03 -0300

    agr foi

--- js/script.js
+++ js/script.js
@@ -4,21 +4,49 @@ button.addEventListener('click', () => {
     console.log("hello,world");
     AJAX();
 })
-function preencheCampos(json) {
-    //falta passar os campos da api que ele vai pegar
-    //no parentes coloca os campos que estão no HTML
-    //no "." depois do "json", coloca o campo que está na api
-    document.querySelector('input[name=userId]').value = json.userId;
-    document.querySelector('input[name=id]').value = json.id;
-    document.querySelector('input[name=title]').value = json.title;
-    document.querySelector('input[name=completed]').value = json.completed;
+function preencheCampos(jsonArray) {
+    // cria a variavel que coloca todas as informaçoes na div
+    const container = document.getElementById('div');
+
+    // cria a tabela via string
+    let tableHTML = `
+        <table>
+            <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Completed</th>
+                </tr>
+            </thead>
+            <tbody>
+    `;
+
+    // cria as linhas a cada usuario requisitado
+    jsonArray.forEach((json) => {
+        tableHTML += `
+            <tr>
+                <td>${json.userId}</td>
+                <td>${json.id}</td>
+                <td>${json.title}</td>
+                <td>${json.completed}</td>
+            </tr>
+        `;
+    });
+    //fecha a tabela
+    tableHTML += `
+            </tbody>
+        </table>
+    `;
+
+    // insere a tabela na div via innerHTML usando o botão que está usando o addEventListener
+    container.innerHTML = tableHTML;
 }
 function AJAX() {
-    //pega quantos usuários serão criados pela API
-    /*paramos aqui 06/11/24*/
-    let numUsuarios=document.querySelector('input[name=]');
-    //url da api
-    let url = 'https://jsonplaceholder.typicode.com/todos/1';
+    //pega quantos usuários serão requisitados pela API
+    let numUsuarios = document.getElementById('numUsuarios').value;
+    //url da api com o numero dos usuarios a serem requisitados
+    let url = `https://jsonplaceholder.typicode.com/todos?_limit=${numUsuarios}`;
     //instancia o AJAX -> lembra da Juliana
     let xhr = new XMLHttpRequest();
     //passa os paramentros -> isso aqui tbm aprendemos com a Juliana?
@@ -29,5 +57,6 @@ function AJAX() {
             preencheCampos(JSON.parse(xhr.responseText));
         }
     }
+    //envia a requisição para o servidor da API
     xhr.send();
 }
